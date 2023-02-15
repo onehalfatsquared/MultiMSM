removed the get_mass_weighted_monomer() method from the MultiMSM class. This turns out not to be needed if we perform the correct count matrix weighting, since p[0] will then giv the mass-weighted monomer. Added the above mentioned weighting to the MSM class, in the finalize_counts() method. This is currently hard-coded for the trimer system, so this will need refactoring.

MultiMSM/MSM/MultiMSM.py
@@ -282,7 +282,7 @@ class Collection:
         p          = np.zeros((T+1, self.__num_states), dtype=float)
         indices    = np.zeros(T+1, dtype=int)
         p[0, :]    = p0
-        mon_frac0  = self.__get_mass_weighted_monomer(p0)
+        mon_frac0  = p0[self.__monomer_index]
         indices[0] = self.get_msm_index(self.__fix_zero_one(mon_frac0))
 
         #solve the FKE, grabbing the relevant transition matrix each iteration
@@ -295,10 +295,13 @@ class Collection:
             p[t+1, :] = p[t, :] * TM
 
             #get the index for the next transition matrix from monomer frac
-            current_mon_frac = self.__get_mass_weighted_monomer(p[t+1, :])
+            current_mon_frac = p[t+1,self.__monomer_index]
             indices[t+1] = self.get_msm_index(self.__fix_zero_one(current_mon_frac))
-            print(t+1,current_mon_frac, indices[t+1])
-            # print(TM)
+
+            #determine when the monomer fraction hops discretization intervals
+            if indices[t+1] != indices[t]:
+                print(t+1,current_mon_frac, indices[t+1])
+                print(TM)
 
 
         #store the solution and indices
@@ -361,27 +364,6 @@ class Collection:
             mon_frac += 1e-6
 
         return mon_frac
-
-    def __get_mass_weighted_monomer(self, probs):
-        #perform mass-weighting on a probability distribution to get the monomer fraction
-
-        #if this is the first time here, create and store a mapping from state index to size
-        if not self.__sizes_stored:
-
-            for i in range(self.__num_states):
-
-                state = self.__macrostate_map.index_to_state(i)
-                size  = state.get_size() # TODO - this needs to be implemented 
-                self.__index_to_size[i] = size
-
-            #switch the flag to true
-            self.__sizes_stored = True
-
-        #take inner product between probs and index_to_size to get MW normalization
-        S = np.dot(probs, self.__index_to_size)
-
-        #return the monomer index scaled by the MW normalization
-        return probs[self.__monomer_index]/S
 
     def get_effective_MSM(self):
 

MultiMSM/util/sampling_data.py
@@ -186,7 +186,7 @@ class ClusterSizeData:
             row_sums = np.linalg.norm(self.__summed_sizes, axis=1, ord=1)
             self.__size_distribution = self.__summed_sizes / row_sums[:, None]
 
-            #mass weighted
+            #mass weighted - TODO - will eventually need fixing to use size of state
             num_states = self.__summed_sizes.shape[1]
             size_range = np.arange(1, num_states+1)
             summed_sizes_mw = self.__summed_sizes * size_range