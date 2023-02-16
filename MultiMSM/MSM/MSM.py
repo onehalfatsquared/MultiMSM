general cleanup of debug print statement in the MSM and MultiMSM class. Generalized the weighting of the count matrices to the particle perspective by using State objects and the MacrostateMap. Fixes #1.

MultiMSM/MSM/MSM.py
@@ -26,18 +26,18 @@ class MSM:
         self.__count_matrix[a,b] += counts
         return
 
-    def finalize_counts(self):
+    def finalize_counts(self, macrostate_map):
         #after all counts are added, convert to a csr matrix and compute row sums
         #use these to construct a row normalized probability transition matrix
 
-        #TODO - clean this, make usable with State objects with a size
-        #multiply by a weighting matrix to map cluster counts to particle counts
-        weighting = scipy.sparse.dok_matrix(np.mgrid[1:4, 1:4].min(axis=0))
-        self.__count_matrix = self.__count_matrix.multiply(weighting)
-        
+        #weight the count matrix to correctly represent mass-weighted dynamics
+        self.__perform_weighting(macrostate_map)
+
+        #convert sparse matrix to csr for more efficient arithmetic. get row sums. 
         self.__count_matrix = self.__count_matrix.tocsr()
         self.__row_counts   = np.asarray(self.__count_matrix.sum(axis=1)).squeeze()
 
+        #normalize the rows to construct a transition matrix
         with warnings.catch_warnings():
             warnings.simplefilter('ignore') #sparse efficiency warning, cant be avoided?
             self.__compute_probability_matrix()
@@ -73,10 +73,10 @@ class MSM:
 
         return self.__lag
 
-    def set_count_matrix(self, count_matrix):
+    def set_count_matrix(self, count_matrix, macrostate_map):
 
         self.__count_matrix = count_matrix
-        self.finalize_counts()
+        self.finalize_counts(macrostate_map)
 
         return
 
@@ -127,3 +127,31 @@ class MSM:
             p[t+1, :] = p[t, :] * TM
 
         return p
+
+    def __perform_weighting(self, macrostate_map):
+        ''' 
+        Multiply the count matrix elementwise by min(size(i),size(j)). 
+        Converts from a cluster perspective (SAASH) to particle perspective,
+        needed to correctly characterize mass-weighted dynamics. 
+        '''
+
+        #get the nonzero indices and number of nonzero elements
+        nzR, nzC = self.__count_matrix.nonzero()
+        nnz = len(nzR)
+
+        #loop over each non-zero, multiply by proper weighting
+        for i in range(nnz):
+
+            #grab the row and column index of the current entry
+            row_index = nzR[i]
+            col_index = nzC[i]
+
+            #convert these state indices to States and grab their size
+            row_state_size = macrostate_map.index_to_state(row_index).get_size()
+            col_state_size = macrostate_map.index_to_state(col_index).get_size()
+
+            #multiply C[r][c] by the weighting min(r_size, c_size)
+            W = min(row_state_size, col_state_size)
+            self.__count_matrix[row_index, col_index] *= W
+
+        return

MultiMSM/MSM/MultiMSM.py
@@ -68,10 +68,6 @@ class Collection:
 
         self.__counts_finalized = False
 
-        #init storage and a flag for mapping from state index to size
-        self.__index_to_size = np.zeros(self.__num_states, dtype=int)
-        self.__sizes_stored  = False
-
         return
 
     def add_transition(self, start_state, end_state, monomer_fraction):
@@ -116,7 +112,6 @@ class Collection:
                 #get list of all transitions within the lag interval, and the monomer fraction at that time
                 transitions = traj.get_transitions(start, lag)
                 mon_frac    = traj.get_data()[start]['monomer_fraction']
-                # print(transitions)
 
                 #add them one by one to the dict
                 for transition in transitions:
@@ -162,7 +157,7 @@ class Collection:
 
         for i in range(self.__num_elements):
 
-            self.__MSM_map[i+1].finalize_counts()
+            self.__MSM_map[i+1].finalize_counts(self.__macrostate_map)
 
 
         #set a flag that there are now transition matrices that can be used
@@ -298,12 +293,6 @@ class Collection:
             current_mon_frac = p[t+1,self.__monomer_index]
             indices[t+1] = self.get_msm_index(self.__fix_zero_one(current_mon_frac))
 
-            #determine when the monomer fraction hops discretization intervals
-            if indices[t+1] != indices[t]:
-                print(t+1,current_mon_frac, indices[t+1])
-                print(TM)
-
-
         #store the solution and indices
         self.__fke_soln    = p
         self.__msm_indices = indices
@@ -375,6 +364,6 @@ class Collection:
             count_matrix += self.get_msm(key).get_count_matrix()
 
         msm = MSM(num_states, lag=self.__lag)
-        msm.set_count_matrix(count_matrix)
+        msm.set_count_matrix(count_matrix, self.__macrostate_map)
 
         return msm